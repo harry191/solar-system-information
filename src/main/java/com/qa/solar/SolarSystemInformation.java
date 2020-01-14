0aa5f470c8c94de014bb0b684c9804f3d1b08874
H <thisisafakeemail@lol.com>
2020-01-14 10:30:25 +0000

    commit

--- src/main/java/com/qa/solar/SolarSystemInformation.java
+++ src/main/java/com/qa/solar/SolarSystemInformation.java
@@ -2,4 +2,26 @@ package com.qa.solar;
 
 public class SolarSystemInformation {
 
+	private String userID;
+	private String password;
+	private Boolean length;
+	
+
+	public SolarSystemInformation(String userID, String password) {
+		if (userID.length() == 6) {
+			length = true;
+		}else {
+			length = false;
+		}
+		
+	}
+	
+	public String lengthCheck(){
+		if (length == true) {
+			return ("Length check passed");
+		}else {
+			return ("Length check failed");
+		}
+	}
+	
 }
---
+++ src/test/java/com/qa/solar/SolarSystemInformationTest.java
@@ -0,0 +1,22 @@
+package com.qa.solar;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolarSystemInformationTest {
+
+	@Test
+	void CheckIfFirstTwoCharactersAreAlphaUpperCase() {
+		
+		String input = ("XX3243");
+		
+		SolarSystemInformation ssi = new SolarSystemInformation(input, input);
+		
+		String result = ssi.lengthCheck();
+		String expected = ("Length check passed");
+		assertEquals(expected, result);
+	}
+
+}
